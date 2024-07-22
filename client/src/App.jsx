7f4eabfd964143f8a4495bf5466ff5b59dbe6a0e
Iva <141308984+Iveto97@users.noreply.github.com>
2024-07-22 15:36:32 +0300

    Add game details link and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
+import {Routes, Route} from 'react-router-dom';
+
 import Header from "./components/header/Header"
 import Home from "./components/home/Home";
-import {Routes, Route} from 'react-router-dom'
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
 import GameCatalogue from "./components/game-catalogue/GameCatalogue";
 import GameCreate from "./components/game-create/GameCreate";
+import GameDetails from "./components/game-details/GameDetails";
 
 function App() {
 
@@ -19,6 +21,8 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/games" element={<GameCatalogue />} />
+            <Route path="/games/:gameId" element={<GameDetails />} />
+
             <Route path="/game-create" element={<GameCreate />} />
            </Routes>
         </main>
