311df8ca1159432200dbd3eb63d1db2adb05f908
Iva <141308984+Iveto97@users.noreply.github.com>
2024-07-29 10:39:37 +0300

    Add one total export

--- client/src/api/requester.js
+++ client/src/api/requester.js
@@ -15,6 +15,7 @@ async function requester(method, url, data) {
 
     const response = await fetch(url, options);
     const result = await response.json();
+
     return result;
 }
 
@@ -22,3 +23,10 @@ export const get = requester.bind(null, 'GET');
 export const post = requester.bind(null, 'POST');
 export const put = requester.bind(null, 'PUT');
 export const del = requester.bind(null, 'DELETE');
+
+export default {
+    get,
+    post,
+    put,
+    del
+};
