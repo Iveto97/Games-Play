a446310af30bd548624b0ec138a96e952f43146e
Iva <141308984+Iveto97@users.noreply.github.com>
2024-08-05 09:22:30 +0300

    Add getAccessToken util

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -7,6 +7,7 @@ export const AuthContext = createContext({
     accessToken: '',
     isAuthenticated: false,
     changeAuthState: (authState = {}) => null,
+    logout: () => null,
 });
 
 export function AuthContextProvider(props) {
@@ -14,18 +15,20 @@ export function AuthContextProvider(props) {
 const [authState, setAuthState] = usePersistedState('auth', {});
 
 const changeAuthState = (state) => {
-  //TODO: Quick solution, fix by implementing persisted authState
-  localStorage.setItem('accessToken', state.accessToken);
-
 	setAuthState(state);
 };
 
+const logout = () => {
+  setAuthState(null);
+}
+
 const contextData = {
-  userId: authState._id,
-	email: authState.email,
-	accessToken: authState.accessToken,
-	isAuthenticated: !!authState.email,
+  userId: authState?._id,
+	email: authState?.email,
+	accessToken: authState?.accessToken,
+	isAuthenticated: !!authState?.email,
 	changeAuthState,
+  logout,
 };
     return (
         <AuthContext.Provider value={contextData}>
